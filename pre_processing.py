5c801e380be98487beed77c0a392ae55d07b582d
Ömer Bayrak <omersocial8@gmail.com>
2022-05-17 22:50:30 +0300

    Create pre_processing.py

---
+++ pre_processing.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    
+#erosion
+def erode(image):
+    kernel = np.ones((2,2),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
